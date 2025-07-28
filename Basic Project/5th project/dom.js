90c0b92eeb7b772372abe7a91f103f96e051621a
Anup shahi <bhanushahi70@gmail.com>
2025-07-28 18:47:19 +0530

    done every changes

---
+++ Basic Project/5th project/dom.js
@@ -0,0 +1,29 @@
+// first we make a function 
+// hex contain hex up to 16 number
+// to change color we use # and then use for loop
+
+const randomcolor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+let intervalid;    // this is use for global variable it can access out of functions 
+const startChangingColor = function () {
+    if (!intervalid) {
+        intervalid = setInterval(chnageBgcolor, 1000);         //we used condition here 
+    }
+    function chnageBgcolor() {
+        document.body.style.backgroundColor = randomcolor();        // this is use to change color
+    }
+};
+const stopChanginColor = function () {
+    clearInterval(intervalid);                            // this is for clear the interval or stop 
+    intervalid = null;                                    // we use null to perform over code better
+};
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChanginColor);
+    
