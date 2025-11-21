452d457793a8d5b37ac7bb6f9631f09d447d03a5
Anup shahi <bhanushahi70@gmail.com>
2025-11-21 20:57:18 +0530

    added comments in dom project

--- Basic Project/5th project/dom.js
+++ Basic Project/5th project/dom.js
@@ -14,16 +14,17 @@ const randomcolor = function () {
 let intervalid;    // this is use for global variable it can access out of functions 
 const startChangingColor = function () {
     if (!intervalid) {
-        intervalid = setInterval(chnageBgcolor, 1000);         //we used condition here 
+        intervalid = setInterval(chnageBgcolor, 1000);         //we used condition here after every 1 sec it change the color
     }
     function chnageBgcolor() {
-        document.body.style.backgroundColor = randomcolor();        // this is use to change color
+        document.body.style.backgroundColor = randomcolor();        // this is use to change color and call the above function 
     }
 };
 const stopChanginColor = function () {
     clearInterval(intervalid);                            // this is for clear the interval or stop 
     intervalid = null;                                    // we use null to perform over code better
 };
+
 document.querySelector('#start').addEventListener('click', startChangingColor);
 document.querySelector('#stop').addEventListener('click', stopChanginColor);
     
